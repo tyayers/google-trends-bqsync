52aadd65f94dabdc393055fcb5c888d64e150b4e
tyayers <tyler.ayers@gmail.com>
2023-01-10 06:43:10 +0100

    updated tf with vars

--- tf/main.tf
+++ tf/main.tf
@@ -1,3 +1,13 @@
+variable "project_id" {
+  description = "Project id (also used for the Apigee Organization)."
+  type        = string
+}
+
+variable "region" {
+  description = "GCP region for storing firebase data (see https://cloud.google.com/apigee/docs/api-platform/get-started/install-cli)."
+  type        = string
+}
+
 resource "google_app_engine_application" "app" {
   project     = var.project_id
   location_id = var.region
