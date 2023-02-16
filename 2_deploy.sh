1e874f77eb55d6656634cdf7d96da2d906658355
tyayers <tyler.ayers@gmail.com>
2023-02-16 12:35:45 +0100

    updated

--- 2_deploy.sh
+++ 2_deploy.sh
@@ -1,4 +1,4 @@
 cd tf
-terraform apply -var "project_id=cloud32x" -var "data_region=europe-west" -var "region=europe-west1"
+terraform apply -var "project_id=cloud32x" -var "project_create=true" -var "data_region=europe-west" -var "region=europe-west1"
 
 cd ..
