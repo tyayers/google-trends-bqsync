7f0c05996218356815b97a03de9a9f55a8547f25
tyayers <tyler.ayers@gmail.com>
2023-02-16 15:06:30 +0100

    updated

--- 2_deploy.sh
+++ 2_deploy.sh
@@ -1,4 +1,6 @@
 cd tf
-terraform apply -var "project_id=cloud32x" -var "project_create=true" -var "data_region=europe-west" -var "region=europe-west1"
+
+terraform init
+terraform apply -var "project_id=PROJECT_ID" -var "data_region=europe-west" -var "region=europe-west1"
 
 cd ..
--- tf/main.tf
+++ tf/main.tf
@@ -15,89 +15,27 @@ variable "region" {
   type        = string
 }
 
-variable "billing_account" {
-  description = "Billing account id."
-  type        = string
-  default     = null
-}
-
-variable "project_parent" {
-  description = "Parent folder or organization in 'folders/folder_id' or 'organizations/org_id' format."
-  type        = string
-  default     = null
-  validation {
-    condition     = var.project_parent == null || can(regex("(organizations|folders)/[0-9]+", var.project_parent))
-    error_message = "Parent must be of the form folders/folder_id or organizations/organization_id."
-  }
-}
-
-variable "project_create" {
-  description = "Create project. When set to false, uses a data source to reference existing project."
-  type        = bool
-  default     = false
-}
-
 # Resources
 
-# Project
-module "project" {
-  source          = "github.com/terraform-google-modules/cloud-foundation-fabric//modules/project?ref=v15.0.0"
-  name            = var.project_id
-  parent          = var.project_parent
-  billing_account = var.billing_account
-  project_create  = var.project_create
-  services = [
-    "compute.googleapis.com",
-    "artifactregistry.googleapis.com",
-    "cloudscheduler.googleapis.com",
-    "run.googleapis.com",
-    "bigquery.googleapis.com"
-  ]
-  policy_boolean = {
-    "constraints/compute.requireOsLogin" = false
-    "constraints/compute.requireShieldedVm" = false
-  }
-  policy_list = {
-    "constraints/iam.allowedPolicyMemberDomains" = {
-        inherit_from_parent: false
-        status: true
-        suggested_value: null
-        values: [],
-        allow: {
-          all=true
-        }
-    },
-    "constraints/compute.vmExternalIpAccess" = {
-        inherit_from_parent: false
-        status: true
-        suggested_value: null
-        values: [],
-        allow: {
-          all=true
-        }
-    }
-  }
+resource "google_project_service" "enable_artifactregistry" {
+  project = var.project_id
+  service = "artifactregistry.googleapis.com"
 }
 
-# resource "google_project_service" "enable_artifactregistry" {
-#   project = var.project_id
-#   service = "artifactregistry.googleapis.com"
-# }
+resource "google_project_service" "enable_scheduler" {
+  project = var.project_id
+  service = "cloudscheduler.googleapis.com"
+}
 
-# resource "google_project_service" "enable_scheduler" {
-#   project = var.project_id
-#   service = "cloudscheduler.googleapis.com"
-# }
+resource "google_project_service" "enable_cloudrun" {
+  project = var.project_id
+  service = "run.googleapis.com"
+}
 
-# resource "google_project_service" "enable_cloudrun" {
-#   project = var.project_id
-#   service = "run.googleapis.com"
-# }
-
-# resource "google_project_service" "enable_bigquery" {
-#   project = var.project_id
-#   service = "bigquery.googleapis.com"
-# }
+resource "google_project_service" "enable_bigquery" {
+  project = var.project_id
+  service = "bigquery.googleapis.com"
+}
 
 resource "google_app_engine_application" "app" {
   project     = var.project_id
@@ -123,6 +61,10 @@ resource "google_artifact_registry_repository" "trends-registry" {
   repository_id = "trends-registry"
   description   = "Registry for trends artifacts"
   format        = "DOCKER"
+  
+  provisioner "local-exec" {
+    command = "gcloud builds submit --config=cloudbuild.yaml --substitutions=_LOCATION='${var.region}',_REPOSITORY='trends-registry',_IMAGE='trends-service' ."
+  }
 }
 
 resource "google_cloud_run_service" "trends_admin_service" {
